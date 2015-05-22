713adee3477d884030f58403587e97e21c7028df
Timothy York <timothypyork@gmail.com>
2015-05-22 15:53:50 -0400

    working code to define DMRs

--- findDMR.R
+++ findDMR.R
@@ -22,28 +22,43 @@ temp <- data.frame(cg= paste('cg',1:5,sep=''), chr= rep(1,5), position= c(2000,5
 
 # FIND OVERLAPPING POSITIONS ---------------------------------------------
 
-k <- 1000
-count <- 0
-out <- data.frame(region=NA, chr=NA, start=NA, length=NA, num_cpg=NA, area=NA)
+k <- 300
+count <- 1
+out <- data.frame(region=NA, chr=NA, start=NA, stop=NA, length=NA, num_cpg=NA)
+clean <- TRUE
 
 for (i in 1:dim(temp)[1]) {
-  count <- count + 1
-  if (temp[i,'position']+k >= temp[i+1,'position']) {
-    count <- count + 1
-    out[count,'region'] <- count
-    out[count,'chr'] <- temp[i,'chr']
-    out[count,
-    num_cpgs <- 
-  
-  } else {
-    out[count,'region'] <- count
-    out[count,'chr'] <- temp[i,'chr']
-    out[count,
-    
-
-
+  #initialize a new region here
+  if (clean) {
+    new <- data.frame(region=NA, chr=NA, start=NA, stop=NA, length=NA, num_cpg=NA)    
+    cpgs <- 1
+    new$region <- count
+    new$chr <- temp[i,'chr']
+    new$start <- temp[i,'position']
+    new$stop <- temp[i,'position']
+    new$length <- new$stop - new$start
+    new$num_cpg <- 1
+    point <- temp[i,'position']
   }
 
+  if (i==dim(temp)[1]) {
+    out <- rbind(out, new)
+    out <- out[-1,]
+    break
+  }
+
+  if (point+k >= temp[i+1,'position']) {
+    new$stop <- temp[i+1,'position']
+    new$length <- new$stop - new$start
+    new$num_cpg <- new$num_cpg + 1
+    point <- new$stop
+    clean <- FALSE
+  
+  } else {
+    out <- rbind(out, new)    
+    clean <- TRUE
+    count <- count + 1
+  }
 
 }
 
