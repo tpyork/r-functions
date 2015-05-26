0c8451f05641b03df9d869ab037cb27875c0a6dc
Timothy York <timothypyork@gmail.com>
2015-05-26 09:29:16 -0400

    Add chromosome specific DMR

--- findDMR.R
+++ findDMR.R
@@ -16,19 +16,23 @@ load("CaB_Pre_Post_Me.Rdata")
 
 # SIMULATE DATA ----------------------------------------------------------
 
-temp <- data.frame(cg= paste('cg',1:5,sep=''), chr= rep(1,5), position= c(2000,5000,5500,5525,8000), statistic= c(8,3.6, 5, 5.5, 3))
+temp <- data.frame(cg= paste('cg',1:11,sep=''), chr= c(rep(1,7), rep(2,4)), position= c(2000,5000,5500,5525,8000,8100,8500, 2000,5000,5500,9000), statistic= c(8,3.6, 5, 5.5, 3, 4,4.5, 3, 4, 2.5, 7))
 
 
 
 # FIND OVERLAPPING POSITIONS ---------------------------------------------
+# data must be ordered by position
 
-k <- 300
+k <- 1000
 count <- 1
 out <- data.frame(region=NA, chr=NA, start=NA, stop=NA, length=NA, num_cpg=NA)
 clean <- TRUE
+chr.curr <- 1
 
 for (i in 1:dim(temp)[1]) {
   #initialize a new region here
+
+
   if (clean) {
     new <- data.frame(region=NA, chr=NA, start=NA, stop=NA, length=NA, num_cpg=NA)    
     cpgs <- 1
@@ -41,6 +45,7 @@ for (i in 1:dim(temp)[1]) {
     point <- temp[i,'position']
   }
 
+  # end if last CpG
   if (i==dim(temp)[1]) {
     out <- rbind(out, new)
     out <- out[-1,]
@@ -59,9 +64,9 @@ for (i in 1:dim(temp)[1]) {
     clean <- TRUE
     count <- count + 1
   }
-
 }
 
+out
 
 
 
